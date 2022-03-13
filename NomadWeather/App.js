b76100f2a21cc778d702348eddca424f6234d198
knj <rlaskawns888@gmail.com>
2022-03-13 18:55:33 +0900

    weather app styles start

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -1,23 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { View, Text, StyleSheet, Dimensions, ScrollView } from "react-native";
+
+const { width : SCREEN_WIDTH  } = Dimensions.get("window");
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Hello World!! For Real?!......</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+
+      <ScrollView 
+        horizontal 
+        pagingEnabled 
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={styles.weather}
+      >
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>Sunny</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>Sunny</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>Sunny</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>Sunny</Text>  
+        </View>  
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>  
+          <Text style={styles.description}>Sunny</Text>  
+        </View>  
+      </ScrollView>
+
+    </View>    
+  )
+}
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  text : {
-    fontSize : 12,
-  },
+  container : { flex : 1, backgroundColor : "tomato" },   
+  city : { flex : 1, justifyContent : "center", alignItems: "center" },
+  cityName : { fontSize : 48, fontWeight : "500" },
+  weather : { },
+  day : { width: SCREEN_WIDTH, alignItems: "center"},
+  temp: { marginTop: 50, fontSize: 178 }, 
+  description : { marginTop: -30, fontSize: 60 },
 });
