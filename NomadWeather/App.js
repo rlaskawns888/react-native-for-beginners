e3693e6170f4fb5dd96b76216c626591d007f00b
knj <rlaskawns888@gmail.com>
2022-03-13 13:49:27 +0900

    React Native 2.3 Thrid Party Packges

--- NomadWeather/App.js
+++ NomadWeather/App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World!! For Real?!</Text>
+      <Text style={styles.text}>Hello World!! For Real?!......</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +16,8 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text : {
+    fontSize : 12,
   },
 });
